fc16e7027b984ea7609c8d3b7a83e4aa95392625
Hamish Fagg <hamish@neonsurge.com>
2022-06-16 14:32:32 +1200

    verbose entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+echo "\n\n--- export ---"
+export
 
+echo "\n\n--- running update-kubeconfig ---"
 aws eks --region ${AWS_REGION} update-kubeconfig --name ${CLUSTER_NAME}
+
+echo "\n\n--- kube config ---"
+echo `cat ~/.kube/config`
+
+echo "\n\n--- caller identity ---"
+aws sts get-caller-identity
+
+echo "\n\n--- get pods ---"
 kubectl get pods
+
+echo "\n\n--- helm list ---"
 helm list
